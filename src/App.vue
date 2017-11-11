1ee68a2845efe90433274fd181c9880e530d8bde
Evgen <evgen.maevskiy@gmail.com>
2017-11-11 16:36:29 +0300

    start point

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,11 @@
 <template>
-    <div class="base">
-        <appHeader :counter="quotesCounter" :progress="progress"></appHeader>
-        <newQuote :quotes="quotes"></newQuote>
-        <quotes :quotes="quotes"></quotes>
+    <div class="conteiner">
+
     </div>
 </template>
 
 <script>
-    import Header from './components/header.vue'
-    import newQuote from './components/newQuote.vue'
-    import Quotes from './components/quotes.vue'
-    import { bus } from './main'
 
-    export default {
-        data() {
-          return {
-            quotes: ['This is test quote','This is test quote', 'This is test quote']
-          }
-        },
-        computed: {
-          quotesCounter() {
-            return this.quotes.length
-          },
-          progress(){
-            return this.quotesCounter + '0'
-          }
-        },
-        components: {
-          appHeader: Header,
-          newQuote,
-          Quotes
-        },
-        created() {
-          bus.$on('deletedQuote', (quote) => {
-            this.quotes.splice(quote, 1)
-        })
-        }
-    }
 </script>
 
 <style scoped>
